aba6db32420e97a8c35017b35fde1c453919a0b6
KaeloPlay <102792246+KaeloPlay@users.noreply.github.com>
2026-01-02 18:47:43 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -8,39 +8,39 @@ const list = [
 { nama: "Orang", nilai: 84 },
 { nama: "Orang", nilai: 82 },
 { nama: "Orang", nilai: 70 },
+{ nama: "Orang", nilai: 68 },
 ];
+
+window.onload = () => {
+	document.querySelector(".elements").style.opacity = 1;
+	showLeaderboard(list)
+}
 
 const lbOutputOri = document.querySelector("#lbOutput");
 const parent = document.querySelector("#outputContainer");
-
-showLeaderboard(list)
 
 function showLeaderboard(data) {
 	data.sort((a, b) => b.nilai - a.nilai);
 	
 	let result;
-	let i1 = 0;
 	for (let i = 0; i < data.length; i++) {
 		const rank = i + 1;
 		const lbOutputClone = lbOutputOri.cloneNode(false);
-		parent.appendChild(lbOutputClone);
+		
 		lbOutputClone.removeAttribute("id");
 		
-		if (i < 3) {
-			result = `${rank}.  |  ${data[i].nama}  |  ${data[i].nilai}`;
+		result = i < 3 ? `${rank}.  |  ${data[i].nama}  |  ${data[i].nilai}` : `<small>${rank}.  |  ${data[i].nama}  |  ${data[i].nilai}</small>`;
 			
-			lbOutputClone.innerHTML = result;
-			lbOutputClone.classList.add("rank" + rank);
-			
-		} else {
-			result = `<small>${rank}.  |  ${data[i].nama}  |  ${data[i].nilai}</small>`;
-			
-			lbOutputClone.innerHTML = result;
-			lbOutputClone.classList.add("other-rank");
-		};
-	};
-};
-
-window.onload = () => {
-	document.querySelector(".elements").style.opacity = 1;
+		lbOutputClone.innerHTML = result;
+		lbOutputClone.classList.add(i < 3 ? "rank" + rank : "other-rank");
+		
+		parent.appendChild(lbOutputClone);
+		
+	}
+	const allChild = document.querySelectorAll(".rank1, .rank2, .rank3, .other-rank");
+	allChild.forEach((rankEl, i) => {
+		setTimeout(() => {
+			rankEl.classList.add("show");
+			}, i * 150);
+		});
 }
